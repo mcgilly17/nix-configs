30b1d9e3fafb6c8465385ee8a412a83d292c6556
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2025-05-25 07:17:28 -0700

    fix: removing altstore temporarily as 1.7.3 doesnt work

--- modules/darwin/apps/desktop.nix
+++ modules/darwin/apps/desktop.nix
@@ -2,7 +2,7 @@
   ### Desktop only Apps
   homebrew = {
     casks = [
-      "altserver" # Server for signging Apollo to my IOS
+      # "altserver@1.7.2" # Server for signging Apollo to my IOS
       "crystalfetch" # ISO fetch for windows and UTM
       "steam"
     ];
