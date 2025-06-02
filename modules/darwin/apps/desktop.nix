4247b3c3d1ae530c7134bc20a3ea41b5ef1a28e0
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2025-06-02 15:24:51 -0700

    update: re-add altserver

--- modules/darwin/apps/desktop.nix
+++ modules/darwin/apps/desktop.nix
@@ -2,7 +2,7 @@
   ### Desktop only Apps
   homebrew = {
     casks = [
-      # "altserver@1.7.2" # Server for signging Apollo to my IOS
+      "altserver" # Server for signging Apollo to my IOS
       "crystalfetch" # ISO fetch for windows and UTM
       "steam"
     ];
