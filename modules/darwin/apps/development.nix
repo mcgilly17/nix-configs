2b36f552e4b204b5872c3c6cb232e2e03d342a9f
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2025-07-07 11:18:05 -0700

    update: add linear

--- modules/darwin/apps/development.nix
+++ modules/darwin/apps/development.nix
@@ -7,6 +7,7 @@
     casks = [
       "godot"
       ## Development
+      "linear-linear"
 
       # "google-cloud-sdk" #The Cloud SDK is a set of tools for Cloud Platform. It contains gcloud, gsutil, and bq, which you can use to access Google Compute Engine https://cloud.google.com/sdk/"
       # "iterm2" #iTerm2 is a replacement for Terminal and the successor to iTerm https://www.iterm2.com/"
