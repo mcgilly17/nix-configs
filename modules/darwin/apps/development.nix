24a5114a4bf6214abecfcfb9899cbe9189ce2e32
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2025-07-12 16:53:34 -0700

    update: add nfov - an nfo viewer

--- modules/darwin/apps/development.nix
+++ modules/darwin/apps/development.nix
@@ -10,6 +10,7 @@
       "linear-linear"
       "claude"
       "granola"
+      "nfov"
 
       # "google-cloud-sdk" #The Cloud SDK is a set of tools for Cloud Platform. It contains gcloud, gsutil, and bq, which you can use to access Google Compute Engine https://cloud.google.com/sdk/"
       # "iterm2" #iTerm2 is a replacement for Terminal and the successor to iTerm https://www.iterm2.com/"
