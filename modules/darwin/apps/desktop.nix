5f163b05cd32d6b17a92c37f3b4d9b23f0f7cc97
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2025-07-12 17:05:26 -0700

    update: add in focusrite controller
    Signed-off-by: McGilly17 <4136843+mcgilly17@users.noreply.github.com>

--- modules/darwin/apps/desktop.nix
+++ modules/darwin/apps/desktop.nix
@@ -5,6 +5,7 @@
       "altserver" # Server for signging Apollo to my IOS
       "crystalfetch" # ISO fetch for windows and UTM
       "steam"
+      "focusrite-control" # controller for scarlett 18i8
     ];
   };
 }
