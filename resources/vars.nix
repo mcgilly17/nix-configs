7dd5c2ac73a6788238d1c172779c18a68f0a27a6
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2025-12-28 22:27:52 -0800

    fix(vars): hardcode user info, remove nix-secrets dependency
    Non-secret info (name, email) shouldn't be in nix-secrets flake outputs.
    Actual secrets use sops-nix at runtime, not evaluation time.

--- resources/vars.nix
+++ resources/vars.nix
@@ -1,13 +1,11 @@
-{ inputs, ... }:
-{
+_: {
   users = {
     michael = {
       username = "michael";
       handle = "McGilly17";
       gitEmail = "4136843+mcgilly17@users.noreply.github.com";
-      userFullName = inputs.nix-secrets.michaelFullName;
-      email = inputs.nix-secrets.michaelEmail;
-      # isMinimal = false; # Used to indicate nixos-installer build
+      userFullName = "Michael Gilchrist Paterson";
+      email = "mpaterson192@gmail.com";
     };
   };
 }
