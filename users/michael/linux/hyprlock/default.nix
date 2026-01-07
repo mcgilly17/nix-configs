8f507c8f895d7233f3b40991ef609f198499ad6d
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2026-01-07 00:04:04 -0800

    style(hyprlock): improve lock screen with catppuccin theme and layout
    Enable catppuccin theming, increase time font to 120px, add user
    greeting, improve input field styling with rounded corners.

--- users/michael/linux/hyprlock/default.nix
+++ users/michael/linux/hyprlock/default.nix
@@ -5,6 +5,9 @@
   ...
 }:
 lib.mkIf (osConfig.programs.hyprland.enable or false) {
+  # Use catppuccin theme for hyprlock
+  catppuccin.hyprlock.enable = true;
+
   programs.hyprlock = {
     enable = true;
     settings = {
@@ -16,51 +19,69 @@ lib.mkIf (osConfig.programs.hyprland.enable or false) {
 
       background = [
         {
+          monitor = "";
           path = "screenshot";
           blur_passes = 3;
-          blur_size = 8;
-          noise = 0.01;
+          blur_size = 6;
+          noise = 0.0117;
           contrast = 0.9;
-          brightness = 0.6;
+          brightness = 0.7;
+          vibrancy = 0.2;
+          vibrancy_darkness = 0.0;
         }
       ];
 
       input-field = [
         {
-          size = "250, 50";
-          position = "0, -80";
+          monitor = "";
+          size = "300, 50";
+          position = "0, -120";
           halign = "center";
           valign = "center";
-          monitor = "";
+          outline_thickness = 3;
+          dots_size = 0.25;
+          dots_spacing = 0.2;
           dots_center = true;
+          dots_rounding = -1;
           fade_on_empty = false;
-          font_color = "rgb(205, 214, 244)";
-          inner_color = "rgb(49, 50, 68)";
-          outer_color = "rgb(137, 180, 250)";
-          outline_thickness = 2;
-          placeholder_text = "<i>Password...</i>";
-          shadow_passes = 2;
+          fade_timeout = 1000;
+          placeholder_text = "Enter Password";
+          hide_input = false;
+          rounding = 15;
+          fail_text = "<i>$FAIL</i>";
+          fail_transition = 300;
+          capslock_color = "rgb(249, 226, 175)";
         }
       ];
 
       label = [
         # Time
         {
-          text = "cmd[update:1000] echo \"$(date +\"%H:%M\")\"";
-          color = "rgb(205, 214, 244)";
-          font_size = 90;
-          font_family = "JetBrainsMono Nerd Font";
-          position = "0, 100";
+          monitor = "";
+          text = "$TIME";
+          font_size = 120;
+          font_family = "JetBrainsMono Nerd Font Bold";
+          position = "0, 200";
           halign = "center";
           valign = "center";
         }
         # Date
         {
-          text = "cmd[update:1000] echo \"$(date +\"%A, %d %B\")\"";
-          color = "rgb(186, 194, 222)";
-          font_size = 20;
+          monitor = "";
+          text = "cmd[update:3600000] echo \"$(date +\"%A, %d %B\")\"";
+          font_size = 22;
           font_family = "JetBrainsMono Nerd Font";
-          position = "0, 20";
+          position = "0, 100";
+          halign = "center";
+          valign = "center";
+        }
+        # Greeting
+        {
+          monitor = "";
+          text = "Hi, $USER";
+          font_size = 18;
+          font_family = "JetBrainsMono Nerd Font";
+          position = "0, -40";
           halign = "center";
           valign = "center";
         }
