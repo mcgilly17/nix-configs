7d80426ab1371b99718c79a45dc3cbf818fb310a
McGilly17 <4136843+mcgilly17@users.noreply.github.com>
2026-03-30 21:49:24 -0700

    feat(mosaic): re-enable fugit2 with doCheck fix and updated keybindings
    Skip neovimRequireCheckHook for fugit2 since libgit2 isn't available
    in the build sandbox. Remap keybindings to avoid conflicts with
    diffview (<leader>gg for Fugit, <leader>gD for Fugit Diff).

--- users/michael/common/tui/mosaic/default.nix
+++ users/michael/common/tui/mosaic/default.nix
@@ -9,10 +9,10 @@
         vimAlias = true;
 
         keymaps = [
-          # Addinf Fugit keymaps
+          # Fugit2 keymaps (<leader>gd and <leader>gf are used by diffview)
           {
             mode = [ "n" ];
-            key = "<leader>gf";
+            key = "<leader>gg";
             action = "<cmd>Fugit2<cr>";
             options = {
               silent = true;
@@ -32,7 +32,7 @@
           }
           {
             mode = [ "n" ];
-            key = "<leader>gd";
+            key = "<leader>gD";
             action = "<cmd>Fugit2Diff<cr>";
             options = {
               silent = true;
@@ -42,27 +42,29 @@
           }
         ];
 
-        # # Fugit2 needs to be installed here, this is due to its dependency on
-        # # libgit2 and how it needs to be linked (with the path)
-        # extraPlugins = with pkgs.vimUtils; [
-        #   # NOTE: Manual pkg install, check update
-        #   (buildVimPlugin {
-        #     pname = "fugit2.nvim";
-        #     version = "0.2.1";
-        #     src = pkgs.fetchFromGitHub {
-        #       owner = "SuperBo";
-        #       repo = "fugit2.nvim";
-        #       rev = "96f729a4bdcf44174c6168f4457b9f3f9229efa2";
-        #       sha256 = "sha256-+T/fiPQDRThyeDuzlwilSWFLW3asoG2gWALy+qVIMZQ=";
-        #     };
-        #   })
-        # ];
-        # extraConfigLua = ''
-        #   require('fugit2').setup{
-        #     libgit2_path = '${pkgs.libgit2.outPath}/lib/libgit2.${pkgs.libgit2.version}.dylib',
-        #     external_diffview = true,
-        #   }
-        # '';
+        # Fugit2 needs to be installed here, this is due to its dependency on
+        # libgit2 and how it needs to be linked (with the path)
+        extraPlugins = with pkgs.vimUtils; [
+          # NOTE: Manual pkg install, check update
+          (buildVimPlugin {
+            pname = "fugit2.nvim";
+            version = "0.2.1";
+            src = pkgs.fetchFromGitHub {
+              owner = "SuperBo";
+              repo = "fugit2.nvim";
+              rev = "96f729a4bdcf44174c6168f4457b9f3f9229efa2";
+              sha256 = "sha256-+T/fiPQDRThyeDuzlwilSWFLW3asoG2gWALy+qVIMZQ=";
+            };
+            # libgit2 isn't available in the build sandbox
+            doCheck = false;
+          })
+        ];
+        extraConfigLua = ''
+          require('fugit2').setup{
+            libgit2_path = '${pkgs.libgit2.outPath}/lib/libgit2.${pkgs.libgit2.version}.dylib',
+            external_diffview = true,
+          }
+        '';
       };
     })
   ];
